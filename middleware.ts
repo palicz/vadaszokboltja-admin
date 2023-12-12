72a83b51fc9ee559a167c60c95cb22fcd2e704a2
palicz <hdtoki@gmail.com>
2023-12-12 17:55:20 +0100

    Update public routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-      publicRoutes: ["/api/:path*"],
+      publicRoutes: ["/api/:path*", "/e8cc7cb1-8fb3-46d2-8230-1514f2a88cec"],
 });
 
 export const config = {
